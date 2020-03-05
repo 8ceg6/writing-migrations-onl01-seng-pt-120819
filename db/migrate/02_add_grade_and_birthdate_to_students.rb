eb19bce1db63cea8d2a9802135ffa44b40c892ea
cedric e goddard <8ceg6@comcast.net>
2020-03-05 15:28:13 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,6 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
-  
+  def change
+      add_column :students, :grade, :
+  end 
 end 
